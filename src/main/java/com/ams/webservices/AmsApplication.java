8684a66a26c3f0cf6aa8d050d9927ce565816d49
Dixon Antony <dixonantony@gmail.com>
2020-09-18 17:27:48 +0530

    aws first stable commit

--- src/main/java/com/ams/webservices/AmsApplication.java
+++ src/main/java/com/ams/webservices/AmsApplication.java
@@ -2,9 +2,18 @@ package com.ams.webservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class AmsApplication {
+@ComponentScan("com.ams.webservices")
+public class AmsApplication extends SpringBootServletInitializer{// For AWS
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+           return application.sources(AmsApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmsApplication.class, args);
